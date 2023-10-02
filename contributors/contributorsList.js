93c736fd2f79821860a6186ab4e0c29e00c2003e
Francis wolf <63988443+Francis-Sawyer123@users.noreply.github.com>
2023-10-02 21:17:20 +0800

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2054,4 +2054,9 @@ contributors = [
     fullname: "Mohit Yadav",
     username: "https://github.com/ymohit1603",
   },
+   {
+    id: 424,
+    fullname: "Francis Wolf",
+    username: "https://github.com/Francis-Sawyer123",
+  },
 ];
